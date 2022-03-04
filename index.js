0ea84fe145f1a20da04b0e653b70eba6d7c504e2
Joshua Sushman <jsushman@gmail.com>
2022-03-03 22:22:27 -0800

    Added Procfile and changed port use

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('postData', (request) => {
   if (request.method == 'POST') return ' ' + JSON.stringify(request.body);
@@ -95,7 +98,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
